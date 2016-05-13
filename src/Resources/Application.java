32ae18c2607d15d1445a56ac6d939609690789d0
César Figueroa <cesarabelfigueroa@unitec.edu>
2016-05-12 21:20:20 -0600

    change threads for executors

--- src/Resources/Application.java
+++ src/Resources/Application.java
@@ -3,56 +3,83 @@ package Resources;
 import Structures.LinkedList;
 import Structures.Queue;
 import Structures.Stack;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Application {
 
-    private LinkedList products = new LinkedList();
-    private Stack employees = new Stack();
-    private Queue materials = new Queue();
+    private Queue products = new Queue();
+    private Queue employees = new Queue();
+    private Stack materials = new Stack();
+    Boolean joining = false;
 
     public Application() {
         Material test1 = new Material("test1", "test1", "test1", 4);
         Material test2 = new Material("test2", "test2", "test2", 4);
         Material test3 = new Material("test3", "test3", "test3", 4);
-        products.insert(0, new Product("prueba", "prueba", 5));
-        employees.push(new Employee("César", "0801-1997-22799", 18, "Casamata", 20000.0, this));
-        employees.push(new Employee("Pedro", "0801-1997-22799", 18, "Casamata", 20000.0, this));
-        employees.push(new Employee("Alejadro", "0801-1997-22799", 18, "Casamata", 20000.0, this));
-        ((Product) products.at(0)).getMaterials().insert(0, test1);
-        ((Product) products.at(0)).getMaterials().insert(0, test2);
-        ((Product) products.at(0)).getMaterials().insert(0, test3);
-        materials.queue(test1);
-        materials.queue(test2);
-        materials.queue(test3);
+        Product product1 = new Product("prueba1", "prueba", 5);
+        Product product2 = new Product("prueba2", "prueba", 5);
+        Product product3 = new Product("prueba3", "prueba", 5);
+        product1.getMaterials().insert(0, test2);
+        product1.getMaterials().insert(0, test1);
+        product1.getMaterials().insert(0, test3);
+        products.queue(product1);
+        products.queue(product2);
+        products.queue(product3);
+        Employee person1 = new Employee("César", "0801-1997-22799", 18, "Casamata", 20000.0);
+        Employee person2 = new Employee("Juan", "0801-1997-22799", 18, "Casamata", 20000.0);
+        Employee person3 = new Employee("Pedro", "0801-1997-22799", 18, "Casamata", 20000.0);
+        employees.queue(person1);
+        employees.queue(person2);
+        employees.queue(person3);
+        materials.push(test1);
+        materials.push(test2);
+        materials.push(test3);
     }
 
-    public LinkedList getProducts() {
+    public Queue getProducts() {
         return products;
     }
 
-    public void setProducts(LinkedList products) {
+    public void setProducts(Queue products) {
         this.products = products;
     }
 
-    public Stack getEmployees() {
+    public Queue getEmployees() {
         return employees;
     }
 
-    public void setEmployees(Stack employees) {
+    public void setEmployees(Queue employees) {
         this.employees = employees;
     }
 
-    public Queue getMaterials() {
+    public Stack getMaterials() {
         return materials;
     }
 
-    public void setMaterials(Queue materials) {
+    public void setMaterials(Stack materials) {
         this.materials = materials;
     }
 
     public void produce() {
-        while (!employees.isEmpty()) {
-            ((Thread) employees.pop()).start();
+        ExecutorService executor = Executors.newFixedThreadPool(employees.getSize());
+        while (!products.isEmpty()) {
+            if (!employees.isEmpty()) {
+                if (!materials.isEmpty()) {
+                    Product product = (Product) products.dequeue();
+                    Employee employee = (Employee) employees.dequeue();
+                    Material material = (Material) materials.pop();
+                    Runnable product_line = new ProductionLine(employee, product , material);
+                    executor.execute(product_line);
+                    employees.queue(employee);
+                }
+            }
         }
+        executor.shutdown();
+    }
+    
+    
+    public boolean areEnoughMaterials(Product produt, LinkedList Materials){
+        return true;
     }
 }
--- src/Resources/Employee.java
+++ src/Resources/Employee.java
@@ -2,25 +2,26 @@ package Resources;
 
 import Structures.LinkedList;
 import Structures.Queue;
+import Structures.Stack;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class Employee extends Thread {
+public class Employee  {
 
     private String names;
     private String id_number;
     private int age;
     private String address;
     private double salary;
-    Application application;
 
-    public Employee(String names, String id_number, int age, String address, double salary, Application application) {
+
+    public Employee(String names, String id_number, int age, String address, double salary) {
         this.names = names;
         this.id_number = id_number;
         this.age = age;
         this.address = address;
         this.salary = salary;
-        this.application = application;
+    
     }
 
     public String getNames() {
@@ -66,43 +67,5 @@ public class Employee extends Thread {
     @Override
     public String toString() {
         return names;
-    }
-
-    @Override
-    public void run() {
-        execute();
-    }
-
-    public void execute() {
-        Queue materials = application.getMaterials();
-        LinkedList products = application.getProducts();
-        for (int i = 0; i < products.getSize(); i++) {
-            LinkedList recentProductions = new LinkedList();
-            try {
-                Product product = (Product) products.at(i);
-                LinkedList requiredMaterials = product.getMaterials();
-                
-                while (!materials.isEmpty()) {
-                    for (int j = 0; j < requiredMaterials.getSize(); j++) {
-                        if (materials != null && !materials.isEmpty()) {
-                            if (requiredMaterials.find(materials.peek()) != -1) {
-                                if (!materials.isEmpty()) {
-                                    Material material = (Material) materials.dequeue();
-                                    recentProductions.insert(recentProductions.getSize() > 0 ? recentProductions.getSize() - 1 : 0, material);
-                                }
-                            }
-                        }
-                    }
-                }
-                if (requiredMaterials.getSize() == recentProductions.getSize()) {
-                    System.out.println("si ajustaron los materiales");
-                } else {
-                    System.out.println("No ajustaron los materiales");
-                }
-                
-            } catch (Exception ex) {
-                
-            }
-        }
     }
 }
